1950b3e29f3a0f19ee03f8178673984091f1f173
lucasgpulcinelli <lucasgpulcinelli@users.noreply.github.com>
2021-07-21 14:54:18 -0300

    temp sqldo

--- src/sqlDo.c
+++ src/sqlDo.c
@@ -1,22 +1,51 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "sqlDo.h"
+#include "dataframe.h"
 
 
 dataframe* processCommand(command* instruction)
 {
-    printf("From files: \n");
-    for (int i = 0; i < instruction->from.size; i++){
-        printf("\t%s.tsv\n", instruction->from.str[i]);
-    }
-    printf("Where conditions are: \n");
-    for (int i = 0; i < instruction->whereSize; i++){
-        printf("\tField: %s in File: %s.tsv is equal to %s\n", instruction->where[i].place->key, instruction->where[i].place->fileName, instruction->where[i].comparationValue);
-    }
-    printf("Select: \n");
-    for (int i = 0; i < instruction->selectSize; i++)
+    //cria o df com os arquivos do from
+    dataframe** dfs = read_many_dfs(instruction->from.str, instruction->from.size);
+    
+    //aloca as chaves
+    char** out_keys = malloc(sizeof(char*) * instruction->selectSize);
+    if(out_keys == NULL)
     {
-        printf("\tFile: %s.tsv, Field: %s\n", instruction->select[i].fileName, instruction->select[i].key);
+        delete_many_dfs(dfs, instruction->from.size);
+        return NULL;
     }
 
-    return NULL;
+    int got_null = 0;
+    for(int i = 0; i < instruction->selectSize; i++)
+    {
+        out_keys[i] = malloc(sizeof(char) * (strlen(instruction->select[i].key) + 1));
+        if(out_keys[i] == NULL)
+        {
+            got_null = 1;
+            break;
+        }
+        strcpy(out_keys[i], instruction->select[i].key);
+    }
+
+    //cria o df final
+    dataframe* df_out = create_df(out_keys, instruction->selectSize);
+
+    //aloca e seta as chaves do df_out
+    for(int i = 0; i < dfs[0]->rows; i++)
+    {
+        char** rowvalues;
+        for(int j = 0; j < instruction->selectSize; j++)
+        {
+            
+        }
+
+        append_df(df_out, rowvalues);
+    }
+
+    delete_many_dfs(dfs, instruction->from.size);
+    return df_out;
 }
